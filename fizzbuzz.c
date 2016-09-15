a9683d35c095ef9c3e932d2488548754720f0722
Rob Conaway <rconawa2@ford.com>
2016-09-15 12:45:40 -0400

    Reset solution

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -5,13 +5,5 @@
 
 void fizzbuzz(int n, char* buffer)
 {
-    if (n % 3 == 0 && n % 5 == 0)
-        strcpy(buffer, "fizzbuzz");
-    else if (n % 3 == 0)
-        strcpy(buffer, "fizz");
-    else if (n % 5 == 0)
-        strcpy(buffer, "buzz");
-    else
-        sprintf(buffer, "%d", n);
 }
 
